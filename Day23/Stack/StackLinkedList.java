a805c81194938f0b1ff2e5042d3c1603165e6cb6
Shani-001 <sunnysaini6193@gmail.com>
2026-06-12 20:16:24 +0530

    Day23 Completed

---
+++ Day23/Stack/StackLinkedList.java
@@ -0,0 +1,60 @@
+public class StackLinkedList{
+    static class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+          this.data=data;
+          this.next=null;
+        }
+    }
+    public static class Stack{
+       public static Node head;
+
+       //isEmpty
+       public static boolean isEmpty(){
+        return head==null;
+       }
+
+       //push
+       public static void push(int n){
+        Node newNode=new Node(n);
+        if(isEmpty()){
+            head=newNode;
+            return;
+        }
+        newNode.next=head;
+        head=newNode;
+       }
+
+       //pop
+       public static int pop(){
+        if(isEmpty()){
+            return -1;
+        }
+        int top=head.data;
+        head=head.next;
+        return top;
+       }
+
+       //peek
+       public static int peek(){
+        if(isEmpty()){
+            return -1;
+        }
+        return head.data;
+       }
+
+    }
+    public static void main(String[] args) {
+        Stack s=new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        while(!s.isEmpty()){
+           System.out.println(s.peek());
+           s.pop();
+        }
+    }
+}
